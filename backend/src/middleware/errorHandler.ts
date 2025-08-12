7d361073a893aaf9e65a300c89ec614e65a4eac6
LukePa <lukeparton99@gmail.com>
2025-08-12 19:27:07 +0100

    more error handling

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,6 +1,7 @@
 import {NextFunction, type RequestHandler, Response as ExResponse,
     Request as ExRequest,} from "express";
 import {ApiError} from "../controllers/authController";
+import {ValidateError} from "tsoa";
 
 
 export const errorHandler = (err: unknown, req: ExRequest, res: ExResponse, next: NextFunction) => {
@@ -8,6 +9,14 @@ export const errorHandler = (err: unknown, req: ExRequest, res: ExResponse, next
         return res.status(err.statusCode).json({
             name: err.name,
             message: err.message
+        })
+    }
+    
+    if (err instanceof ValidateError) {
+        return res.status(err.status).json({
+            name: err.name,
+            message: err.message,
+            fields: err.fields
         })
     }
 }
